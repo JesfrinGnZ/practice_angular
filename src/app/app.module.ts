77883913948fc0b6ca143aca29aada6d9acd8d1e
JesfrinGnZ <jess2298gnz@gmail.com>
2020-12-26 04:16:27 -0600

    Ejemplo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const routes: Routes = [
+  { path: 'contact', component: ContactoComponent },
+  { path: 'nosotros', component: CuerpoComponent},
+  { path: '', component:Cuerpo2Component, pathMatch: 'full'},//La pagina por defecto si no la ruta es simple
+  { path: '**', redirectTo:'nosotros', pathMatch: 'full'},//La pagina a donde ira si se pone una ruta desconocida
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    EncabezadoComponent,
+    CuerpoComponent,
+    Cuerpo2Component,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]

7955d427555faca65cc2dacd336d7026e32e0f1a
JesfrinGnZ <jess2298gnz@gmail.com>
2020-12-26 01:38:36 -0600

    Ejemplo de directiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Master aplicacion';
   show:boolean = true;
+  cursos:string[]=['html','angular','pphp'];
+  animales:Array<any> =[
+    {tipo:'Perro',nombre:'Lisa',edad:10},
+    {tipo:'Gato',nombre:'Pepon',edad:2},
+    
+  ];
 }
